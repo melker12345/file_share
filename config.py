cf0b08ff86b2a975da729f1e167b46d14deb2287
Melker <memleoberg@gmail.com>
2026-02-22 16:27:02 +0100

    init

---
+++ config.py
@@ -0,0 +1,39 @@
+import os
+
+# Chech path and if empty.
+def check_path(shared_dir):
+    if os.path.exists(shared_dir):
+        return True
+    else:
+        print("Not a valid directory buddy :/")
+        return False
+
+def add_path():
+    shared_dir = input("Enter directory to append new shared dir")
+
+    if check_path(shared_dir):
+        if os.path.getsize("shared_dir.txt") == 0:
+            with open("shared_dir.txt", "w") as f:
+                f.write(shared_dir + "\n")
+                print(shared_dir)
+        else:
+            with open("shared_dir.txt", "a") as f:
+                f.write(shared_dir + "\n")
+                print(shared_dir)
+
+    else:
+        print("Not a valid directory")
+
+def read_list():
+    list = []
+    with open("shared_dir.txt", "r") as f:
+        for i in f:
+            list.append(i)
+    print(list)
+
+
+
+add_path()
+
+read_list()
+
---
+++ main.py
@@ -0,0 +1,19 @@
+# A quick script to send .env files from pc to pc
+
+# The ide:
+# I want a tool that sends a given file to an pre added pc
+
+# Problem:
+# Syncing .envs are enoying.
+
+# Plan:
+# Auto discover over LAN.
+# One-time pairing 6 digit code.
+# Bidirectional transfering channels.
+# Shared root (form wich devices can request .env files).
+# Commands over channels, LIST, REQUEST and SEND.
+# CLI menu
+
+
+
+
